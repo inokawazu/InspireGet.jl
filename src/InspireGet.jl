a798dca505091fe7cdfa18348e47c378342b5c97
Markus-Amano <markus.a.amano@gmail.com>
2022-05-05 00:09:18 +0800

    added Record

--- src/InspireGet.jl
+++ src/InspireGet.jl
@@ -2,6 +2,9 @@ module InspireGet
 
 using HTTP: get
 import HTTP
+import JSON
+import Dates
+import TimeZones: ZonedDateTime
 
 const INSPIRE_API_URL = "https://inspirehep.net/api"
 
@@ -30,6 +33,37 @@ function get_record(identifier_type::AbstractString, identifier_value::AbstractS
              (INSPIRE_API_URL, identifier_type, identifier_value), '/'
             )
        )
+end
+
+function parse_inspire_timestamp(ts::AbstractString)
+    no_ns_ts = replace(ts, r"\.\d*\+"=>"+")
+    return ZonedDateTime(no_ns_ts, Dates.dateformat"y-m-dTH:M:Ss+z")
+end
+
+struct Record
+    id::Int
+    created
+    updated
+    links::Dict{String,String}
+    metadata::Dict
+end
+
+function Record(identifier_type::AbstractString, identifier_value::AbstractString)
+    resp = get_record(identifier_type, identifier_value)
+    return Record(resp)
+end
+
+function Record(resp::HTTP.Messages.Response)
+    json = JSON.parse(String(resp.body))
+
+    id = parse(Int,json["id"])
+    created = parse_inspire_timestamp(json["created"])
+    updated = parse_inspire_timestamp(json["updated"])
+    links = Dict{String, String}(json["links"])
+    metadata = json["metadata"]
+
+    return Record(id, created, updated, links, metadata)
+
 end
 
 end # module
