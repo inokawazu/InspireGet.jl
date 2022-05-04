f7633be8e14688906f77a508a56168094650b434
Markus-Amano <markus.a.amano@gmail.com>
2022-05-04 22:29:40 +0800

    added inspire_get

--- src/InspireGet.jl
+++ src/InspireGet.jl
@@ -1,7 +1,35 @@
 module InspireGet
 
-import Downloads
+using HTTP: get
+import HTTP
 
+const INSPIRE_API_URL = "https://inspirehep.net/api"
 
+const IDENTIFIERS = ("literature", 
+                     "authors", 
+                     "institutions", 
+                     "conferences", 
+                     "seminars", 
+                     "journals", 
+                     "jobs", 
+                     "experiments", 
+                     "data",
+                     "doi",
+                     "arxiv",
+                     "orcid")
+
+function get_record(identifier_type::AbstractString, identifier_value::AbstractString)::HTTP.Messages.Response
+    if !(identifier_type in IDENTIFIERS)
+        throw(DomainError(identifier_type, 
+                          "Not a valid identifier type. \
+                          Valid types are the following: $(join(IDENTIFIERS, " "))."))
+    end
+
+    return get(
+        join(
+             (INSPIRE_API_URL, identifier_type, identifier_value), ','
+            )
+       )
+end
 
 end # module
